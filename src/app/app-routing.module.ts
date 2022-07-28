1acf1ffb4f450f435deba9658c506cf8bb6383b2
Enomis9339 <61394403+Enomis9339@users.noreply.github.com>
2022-07-28 15:16:34 +0200

    redirect to movie detail on card click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailComponent } from './components/movies-list/movie-detail/movie-detail.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 
 const routes: Routes = [
+  { path: '', component: MoviesListComponent },
   { path: 'home', component: MoviesListComponent },
   { path: 'category/:id', component: MoviesListComponent },
+  { path: 'film/:id', component: MovieDetailComponent },
 ];
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  categories: [] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.setCategories();
   }
 
+  setCategories() {
+    this.moviesService.getCategories().subscribe((res: any) => {
+      console.log(res);
+    });
+  }
 }
