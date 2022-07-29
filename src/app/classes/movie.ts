d7e3599fa232678bef81cd5d60b57ac7b1e8f1f1
Enomis9339 <61394403+Enomis9339@users.noreply.github.com>
2022-07-29 17:01:34 +0200

    movie detail added

---
+++ src/app/classes/movie.ts
@@ -0,0 +1,9 @@
+export class Movie {
+  title: string = '';
+  director: string = ' ';
+  duration: number = 0;
+  id: number = 0;
+  path_locandina: string = '';
+  plot: string = '';
+  year: number = 0;
+}
--- src/app/components/movies-list/movie-detail/movie-detail.component.ts
+++ src/app/components/movies-list/movie-detail/movie-detail.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Movie } from 'src/app/classes/movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.css']
+  styleUrls: ['./movie-detail.component.css'],
 })
 export class MovieDetailComponent implements OnInit {
+  id: number = 0;
 
-  constructor() { }
+  movie: Movie = {
+    title: '',
+    director: ' ',
+    duration: 0,
+    id: 0,
+    path_locandina: '',
+    plot: '',
+    year: 0,
+  };
 
+  styleObject: Object = {
+    background:
+      'url(../../../../assets/movie-posters/' + this.movie.path_locandina + ')',
+    'background-size': 'contain',
+  };
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private moviesService: MoviesService
+  ) {}
   ngOnInit(): void {
+    this.getParamID();
   }
 
+  setMovieDetails(id: number) {
+    this.moviesService.getMovieById(id).subscribe((res: any) => {
+      console.log(res);
+      this.movie = res;
+    });
+  }
+
+  getParamID() {
+    this.activeRoute.paramMap.subscribe((params: ParamMap) => {
+      this.activeRoute.params.subscribe((routeParams) => {
+        let id = routeParams['id'];
+        this.id = routeParams['id'];
+        this.setMovieDetails(id);
+      });
+    });
+  }
 }
