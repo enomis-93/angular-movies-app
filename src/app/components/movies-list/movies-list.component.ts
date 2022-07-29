70e9f1813c5afc7f6b39e55898e039b581c6fd1b
Enomis9339 <61394403+Enomis9339@users.noreply.github.com>
2022-07-29 11:41:58 +0200

    render alla movies on view

--- src/app/components/movies-list/movies-list.component.ts
+++ src/app/components/movies-list/movies-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent implements OnInit {
+  id: any = 0;
+  movies: any[] = [];
 
-  constructor() { }
+  constructor(
+    private moviesService: MoviesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.getParamID();
+    this.getAllMoviesData();
   }
 
+  getAllMoviesData() {
+    this.moviesService.getAllMovies().subscribe((res: any) => {
+      console.log(res);
+      this.movies = res;
+    });
+  }
+
+  // leggo l'id dai parametri del'url e lo uso per l'eventuale chiamata alla singola categoria
+  getParamID() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      if (params.get('id')) {
+        this.id = params.get('id');
+      }
+    });
+  }
 }
